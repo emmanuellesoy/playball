5f357691e0fb66f009eb0b29759c9c01698fa1a1
baduel <jesmanrios@gmail.com>
2013-07-17 20:02:07 -0500

    se agrego el maquetado del home y el sigle

---
+++ single.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<section class="contieneTodo">
+	<div class="contieneTodoFondo">
+		<?php get_template_part('single/nota', 'interna'); ?>
+		<?php get_template_part('general/footer', 'general'); ?>
+	</div><!-- Termina contieneTodoFondio -->
+</section><!--Termina contieneTodo -->
+<?php get_footer();?>
